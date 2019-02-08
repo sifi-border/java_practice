2c11801b11eea8c244386bc391c0dc4c6af85cf5
sifi_border <shusaku5296@gmail.com>
2019-02-08 21:44:29 +0900

    Hello Comment

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 //クラスは大文字で初め、ファイル名と一致させる
 public class Hello
 {
+	//mainメソッドを宣言
 	public static void main(String[] args)
 	{
+		//Systemクラスのoutフィールドのprintlnメソッドを呼び出している
 		System.out.println("Hello, Java!");
 	}
 }
