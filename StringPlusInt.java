b61e5f7e50f5f9e4522d18a0a2a4c34cf8ebf025
sifi_border <shusaku5296@gmail.com>
2019-02-08 21:43:45 +0900

    StringPlus

---
+++ StringPlusInt.java
@@ -0,0 +1,12 @@
+public class StringPlusInt
+{
+	public static void main(String[] args)
+	{
+		//IntがStringに変換される？
+		System.out.println("2 + 3 = " + (2+3));
+		System.out.println("2 + 3 = " + 2 + 3 + "?");
+
+		//printfも使える
+		System.out.printf("2 + 3 = %d", 2 + 3);
+	}
+}
